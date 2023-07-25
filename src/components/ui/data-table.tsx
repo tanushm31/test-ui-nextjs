1fe2cebbad364d7f63acd65d700f019d2aaf9294
Tanush Mahajan <tanush.mohina@gmail.com>
2023-07-25 15:36:01 +0530

    Added Pagination to Data Table

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -3,6 +3,7 @@ import {
 	flexRender,
 	getCoreRowModel,
 	useReactTable,
+	getPaginationRowModel,
 } from "@tanstack/react-table";
 
 import {
@@ -13,7 +14,7 @@ import {
 	TableHeader,
 	TableRow,
 } from "~/components/ui/table";
-
+import { Button } from "./button";
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[];
 	data: TData[];
@@ -27,6 +28,7 @@ export function DataTable<TData, TValue>({
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
 	});
 
 	return (
@@ -80,6 +82,24 @@ export function DataTable<TData, TValue>({
 					)}
 				</TableBody>
 			</Table>
+			<div className="flex items-center justify-end space-x-2 py-4 px-3">
+				<Button
+					variant="outline"
+					size="sm"
+					onClick={() => table.previousPage()}
+					disabled={!table.getCanPreviousPage()}
+				>
+					Previous
+				</Button>
+				<Button
+					variant="outline"
+					size="sm"
+					onClick={() => table.nextPage()}
+					disabled={!table.getCanNextPage()}
+				>
+					Next
+				</Button>
+			</div>
 		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,6 +37,96 @@ export default function Home() {
 			status: "processing",
 			email: "m@example.com",
 		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "pending",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "pending",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "pending",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
+		{
+			id: "728ed52f",
+			amount: 100,
+			status: "processing",
+			email: "m@example.com",
+		},
 	];
 
 	return (
